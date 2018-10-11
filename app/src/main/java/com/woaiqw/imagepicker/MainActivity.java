835ad1e3b6d9d3572eb1485914d75391ecb7387f
woaigmz <woaigmz@gmail.com>
2018-10-11 16:25:03 +0800

    add library

--- app/src/main/java/com/woaiqw/imagepicker/MainActivity.java
+++ app/src/main/java/com/woaiqw/imagepicker/MainActivity.java
@@ -4,11 +4,27 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.woaiqw.library.ImagePicker;
+import com.woaiqw.library.listener.ImagePickerResultListener;
+
+public class MainActivity extends AppCompatActivity implements ImagePickerResultListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImagePicker.create().onResult(this);
+
+    }
+
+    @Override
+    public void onSelected() {
+
+    }
+
+    @Override
+    public void onTakePhoto() {
+
     }
 }

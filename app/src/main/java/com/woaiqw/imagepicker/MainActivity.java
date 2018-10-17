eb3dbf9176e788f5fa615b7465f7eb92c757df60
woaigmz <woaigmz@gmail.com>
2018-10-17 19:05:51 +0800

    first test

--- app/src/main/java/com/woaiqw/imagepicker/MainActivity.java
+++ app/src/main/java/com/woaiqw/imagepicker/MainActivity.java
@@ -1,15 +1,24 @@
 package com.woaiqw.imagepicker;
 
+import android.Manifest;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.woaiqw.base.common.PermissionActivity;
+import com.woaiqw.base.utils.PermissionListener;
+import com.woaiqw.library.ImageLoader;
 import com.woaiqw.library.RxImagePicker;
 import com.woaiqw.library.annotation.ResultType;
 import com.woaiqw.library.listener.ImagePickerResultListener;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.List;
+
+public class MainActivity extends PermissionActivity implements View.OnClickListener {
+
+    private String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,23 +32,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        RxImagePicker
-                .in(MainActivity.this)
-                .createFactory()
-                .setGridColumn(4)
-                .setTheme(R.style.AppTheme)
-                .setResultType(ResultType.URI)
-                .onResult(new ImagePickerResultListener() {
-                    @Override
-                    public void onSelected(Object o) {
 
-                    }
+        requestPermissions(permissions, new PermissionListener() {
+            @Override
+            public void onGranted() {
+                RxImagePicker
+                        .in(MainActivity.this)
+                        .createFactory()
+                        .setGridColumn(4)
+                        .setImageLoader(new ImageLoader<ImageView>() {
+                            @Override
+                            public void displayImage(Context context, Object path, ImageView imageView) {
+                                com.woaiqw.base.utils.ImageLoader.loadImage(context, imageView, (String) path);
+                            }
+                        })
+                        .setTheme(R.style.AppTheme)
+                        .setResultType(ResultType.URI)
+                        .onResult(new ImagePickerResultListener() {
+                            @Override
+                            public void onSelected(Object o) {
 
-                    @Override
-                    public void onTakePhoto(Object o) {
+                            }
 
-                    }
-                })
-                .build();
+                            @Override
+                            public void onTakePhoto(Object o) {
+
+                            }
+                        })
+                        .build();
+            }
+
+            @Override
+            public void onDenied(List<String> list) {
+
+            }
+        });
+
     }
 }
